dce1c3abd145884cf02fc69b76932178f8a60464
BogdanShulga <bogdan.shulga.90@gmail.com>
2019-06-28 21:14:15 +0300

    dev: task4 completed

--- task4/src/main/java/Main.java
+++ task4/src/main/java/Main.java
@@ -1,7 +1,10 @@
 package main.java;
 
+import main.java.classes.BanknoteKeeper;
+
 public class Main {
     public static void main(String[] args) {
-        
+        BanknoteKeeper banknoteKeeper = new BanknoteKeeper();
+        System.out.println(banknoteKeeper.withdrawCash(23369.44));
     }
 }
