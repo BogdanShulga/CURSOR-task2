497c0c400441bc12c09f3dcfb3e2e1d80625d7d8
BogdanShulga <bogdan.shulga.90@gmail.com>
2019-06-29 13:50:36 +0300

    dev: added execution time to task4

--- task4/src/main/java/Main.java
+++ task4/src/main/java/Main.java
@@ -4,7 +4,13 @@ import main.java.classes.BanknoteKeeper;
 
 public class Main {
     public static void main(String[] args) {
+
         BanknoteKeeper banknoteKeeper = new BanknoteKeeper();
-        System.out.println(banknoteKeeper.withdrawCash(23369.44));
+
+        long start = System.currentTimeMillis();
+        System.out.println(banknoteKeeper.withdrawCash(2335424524242695565.6465461565465465465456));
+        long end = System.currentTimeMillis();
+
+        System.out.print("Execution time is " + (end - start) / 1000d + " seconds");
     }
 }
--- task4/src/main/java/classes/BanknoteKeeper.java
+++ task4/src/main/java/classes/BanknoteKeeper.java
@@ -1,9 +1,6 @@
 package main.java.classes;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class BanknoteKeeper {
 
